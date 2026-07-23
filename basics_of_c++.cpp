119de84c855f8927073fd25d58eac7736ac9b65a
KrishJindal85 <krish.jindal2031@gmail.com>
2026-07-23 08:05:54 +0530

    Create basics_of_c++.cpp

---
+++ basics_of_c++.cpp
@@ -0,0 +1,100 @@
+#include<iostream>
+using namespace std;
+
+int main() { 
+/*
+    cout<< "i am krishh"<<endl;
+
+
+int a = 123;
+
+cout << a << endl;
+
+char b = 'v';
+cout << b << endl;
+
+
+bool cm = true;
+cout << cm << endl;
+
+float f = 5.24;
+cout << f << endl;
+//float pi = 3.14159f; // Note: 'f' lagana zaroori hai
+float weight = 65.5f;
+
+double d = 1.764;
+cout << d << endl;
+//double distance = 149597870.7; // Koi suffix nahi chahiye
+
+//float aur double mei zada frk nhi h bss suffix aur space ka frk h
+//💡 Short Summary: Agar memory bachani hai aur thodi kam accuracy se
+// kaam chal jayega, toh float use karo. Agar sahi aur accurate results chahiye (jo ki maximum time chahiye hote hain), toh double use karo.
+
+int size = sizeof(a);
+cout << "size of a is :" << size << endl;
+
+
+
+int m = 'h';
+cout << m << endl;
+
+char ch = 104;
+cout << ch << endl; 
+
+
+char ch1 = 123456;
+cout << ch1 << endl;
+
+unsigned int a = -122;
+cout << a << endl;
+
+
+int a = 2/5;
+cout << a << endl;
+
+double b = 2.0/5;
+cout << b << endl;
+
+cout<< 2.0/5 << endl;
+
+
+int a =2;
+int b =3;
+
+bool a1 = (a==b);
+cout<< a1 << endl;
+
+bool a2 = (a<b);
+cout<< a2 << endl;
+
+bool a3 = (a>b);
+cout<< a3 << endl;
+
+bool a4 = (a<=b);
+cout<< a4 << endl;
+
+bool a5 = (a>=b);
+cout<< a5 << endl;
+
+bool a6 = (a!=b);
+cout<< a6 << endl;
+*/
+
+int a = 5;
+cout<< !a << endl;
+
+
+
+
+
+
+return 0;
+
+
+
+
+
+
+
+
+}
