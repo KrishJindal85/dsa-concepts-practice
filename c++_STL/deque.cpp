8f982480ad0d7b13e8bc6c71afc9e52bdfb2a563
KrishJindal85 <krish.jindal2031@gmail.com>
2026-07-26 10:37:47 +0530

    done basics

---
+++ c++_STL/deque.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<deque>
+using namespace std;
+
+int main()
+{
+    deque<int> d;
+
+    d.push_back(1);
+    d.push_front(2);
+
+//d.pop_front()
+//iska mtlb front wala element ud jaega aur d.pop_back() last wala element
+
+cout<<"print first index element->"<<d.at(1)<<endl;
+
+cout<<"front"<<d.front()<<endl;
+cout<<"back"<<d.back()<<endl;
+
+cout<<"empty or not"<<d.empty()<<endl;
+
+cout<<"before erase"<<d.size()<<endl;
+
+d.erase(d.begin(),d.begin()+1);
+
+cout<<"after erase"<<d.size()<<endl;
+
+for(int i:d){
+
+cout<<i<<endl;
+
+}
+
+}
